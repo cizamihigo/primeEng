c9d84bea4fe56092ea5c48bf2a78fec602442b0d
cizamihigo <cizamihigochristian@gmail.com>
2021-02-22 15:43:59 +0200

    homepage start

--- PrimeEngineer/main/views.py
+++ PrimeEngineer/main/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def homepage(request):
+    return HttpResponse("This is my very first home page in <strong> Django</strong> wow!!")
