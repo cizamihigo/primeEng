936874455c212394926366dfa7f1ef994f7c354a
cizamihigo <cizamihigochristian@gmail.com>
2021-02-22 17:35:38 +0200

    Admin Panel

--- PrimeEngineer/main/models.py
+++ PrimeEngineer/main/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
 class Works(models.Model):
     Name = models.CharField(max_length = 200)
     Description = models.TextField()
-    PublishDate = models.DateTimeField("Published date")
+    PublishDate = models.DateTimeField("Published date", default = datetime.now())
     domaine = models.CharField(max_length = 70)
     photo = models.BinaryField()
 
