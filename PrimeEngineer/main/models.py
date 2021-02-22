35ca8d94d73a92921f941324bf1534bb249df9a9
cizamihigo <cizamihigochristian@gmail.com>
2021-02-22 17:01:39 +0200

    Migration on works done by the author of the site

--- PrimeEngineer/main/models.py
+++ PrimeEngineer/main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Works(models.Model):
+    Name = models.CharField(max_length = 200)
+    Description = models.TextField()
+    PublishDate = models.DateTimeField("Published date")
+    domaine = models.CharField(max_length = 70)
+    photo = models.BinaryField()
+
+    def __str__(self):
+        return self.Name
